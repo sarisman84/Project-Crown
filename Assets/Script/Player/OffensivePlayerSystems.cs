d80aa014c7ae35b60b1de6ed824fa02791b2f8e4
Spyro <epicdudeness677@gmail.com>
2019-12-26 03:47:05 +0100

    Initial Unity Commit

---
+++ Assets/Script/Player/OffensivePlayerSystems.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OffensivePlayerSystems : MonoBehaviour
+{
+    //What does this class do?
+
+    //TODO  Basic projectile shooting
+    //TODO  Object Pooler for projectile shooting
+    //TODO  Projectile definitions (like who fired it, what behaivour does the projectile do, etc.)
+
+
+}
---
+++ Assets/Script/Player/PlayerController.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    
+
+    [SerializeField] float shipMoveSpeed = 5f;
+
+
+    Rigidbody2D rb2D;
+    Vector2 velocity;
+    Quaternion previousRotation;
+
+    
+
+    private void Awake() {
+        GetComponents();
+    }
+
+
+    private void Update() {
+        Vector2 input = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        velocity = CalculateShipMovement(input);
+        Debug.DrawRay(transform.position, transform.up * 10, Color.red);
+        
+    }
+    private void FixedUpdate()
+    {
+
+        rb2D.velocity = velocity;
+
+        Quaternion rotation = UpdateLookDirection(true);
+        transform.rotation = rotation;
+    }
+
+     Quaternion UpdateLookDirection(bool smoothRotation)
+    {
+        
+        if (velocity == Vector2.zero) {
+            return previousRotation;
+        }
+        previousRotation = transform.rotation;
+        Quaternion rotation = Quaternion.LookRotation(Vector3.forward, velocity);
+        return (smoothRotation) ? Quaternion.Slerp(transform.rotation, rotation, Time.fixedDeltaTime * 10f) : rotation;
+    }
+
+    Vector2 CalculateShipMovement(Vector2 input){
+        Vector2 velocity = input * shipMoveSpeed;
+        return velocity;
+    }
+
+
+    #region Variable References
+    void GetComponents(){
+        rb2D = GetComponent<Rigidbody2D>();
+    }
+
+    #endregion
+
+}
