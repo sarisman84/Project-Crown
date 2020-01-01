26dac0d6b9ca1b71dc955ad1d05f4dc6f513d316
Spyro <epicdudeness677@gmail.com>
2020-01-01 04:29:28 +0100

    Implemented; [Early Prototypes of Functional Code]
    Created an Implementation that does the following: [Allow one to inherit base classes of both weapons and their ammo in order to activate a specific effect related to said weapon.]

--- Assets/Script/Utility/ObjectPooler.cs
+++ Assets/Script/Utility/ObjectPooler.cs
@@ -2,46 +2,49 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public static class ObjectPooler
-{
-    static Dictionary<int, List<GameObject>> dictionaryOfPooledObjects = new Dictionary<int, List<GameObject>>();
+public static class ObjectPooler {
+    static Dictionary<int, List<GameObject>> dictionaryOfPooledObjects = new Dictionary<int, List<GameObject>> ();
 
-    public static void PoolObject(GameObject desiredObject, int amount, Transform parent){
-        List<GameObject> tempPooledObjectList = new List<GameObject>();
-        for (int i = 0; i < amount; i++)
-        {
-            GameObject clone = MonoBehaviour.Instantiate(desiredObject, parent);
-            tempPooledObjectList.Add(clone);
-            clone.SetActive(false);
+    public static void PoolObject (GameObject desiredObject, int amount, Transform parent) {
+        List<GameObject> tempPooledObjectList = new List<GameObject> ();
+        for (int i = 0; i < amount; i++) {
+            GameObject clone = MonoBehaviour.Instantiate (desiredObject, parent);
+            tempPooledObjectList.Add (clone);
+            clone.SetActive (false);
         }
 
-
-        dictionaryOfPooledObjects.Add(desiredObject.GetInstanceID(), tempPooledObjectList);
+        dictionaryOfPooledObjects.Add (desiredObject.GetInstanceID (), tempPooledObjectList);
     }
 
-
-    public static GameObject GetPooledObject(GameObject desiredObject){
-        List<GameObject> desiredPooledObjects = new List<GameObject>();
-        dictionaryOfPooledObjects.TryGetValue(desiredObject.GetInstanceID(), out desiredPooledObjects);
-        for (int i = 0; i < desiredPooledObjects.Count; i++)
-        {
-            if(!desiredPooledObjects[i].activeInHierarchy) return desiredPooledObjects[i];
+    public static GameObject GetPooledObject (GameObject desiredObject) {
+        List<GameObject> desiredPooledObjects = new List<GameObject> ();
+        dictionaryOfPooledObjects.TryGetValue (desiredObject.GetInstanceID (), out desiredPooledObjects);
+        for (int i = 0; i < desiredPooledObjects.Count; i++) {
+            if (!desiredPooledObjects[i].activeInHierarchy) return desiredPooledObjects[i];
         }
 
         return null;
     }
 
-
-    public static GameObject Dynamic_GetPooledObject(GameObject desiredObject){
-        List<GameObject> desiredPooledObjects = new List<GameObject>();
-        dictionaryOfPooledObjects.TryGetValue(desiredObject.GetInstanceID(), out desiredPooledObjects);
-        for (int i = 0; i < desiredPooledObjects.Count; i++)
-        {
-            if(!desiredPooledObjects[i].activeInHierarchy) return desiredPooledObjects[i];
+    public static GameObject GetPooledObject (int desiredObject) {
+        List<GameObject> desiredPooledObjects = new List<GameObject> ();
+        dictionaryOfPooledObjects.TryGetValue (desiredObject, out desiredPooledObjects);
+        for (int i = 0; i < desiredPooledObjects.Count; i++) {
+            if (!desiredPooledObjects[i].activeInHierarchy) return desiredPooledObjects[i];
         }
 
-        GameObject obj = MonoBehaviour.Instantiate(desiredObject, desiredPooledObjects[0].transform.parent);
-        dictionaryOfPooledObjects[desiredObject.GetInstanceID()].Add(obj);
+        return null;
+    }
+
+    public static GameObject Dynamic_GetPooledObject (GameObject desiredObject) {
+        List<GameObject> desiredPooledObjects = new List<GameObject> ();
+        dictionaryOfPooledObjects.TryGetValue (desiredObject.GetInstanceID (), out desiredPooledObjects);
+        for (int i = 0; i < desiredPooledObjects.Count; i++) {
+            if (!desiredPooledObjects[i].activeInHierarchy) return desiredPooledObjects[i];
+        }
+
+        GameObject obj = MonoBehaviour.Instantiate (desiredObject, desiredPooledObjects[0].transform.parent);
+        dictionaryOfPooledObjects[desiredObject.GetInstanceID ()].Add (obj);
         return obj;
     }
 
---
+++ Assets/Script/Weapon/BaseWeapon.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public delegate void WeaponBehaivour ();
+public abstract class BaseWeapon : MonoBehaviour, IWeapon {
+
+    protected float fireRate, time;
+    public abstract void OnActivationTrigger ();
+
+}
+public abstract class BaseAmmo : MonoBehaviour, IAmmo {
+
+    protected float damage, velocity;
+
+    public abstract float BulletVelocity { get; }
+
+    public abstract void OnCollisionEnter2D (Collision2D other);
+}
+
+public interface IAmmo {
+
+    float BulletVelocity { get; }
+    void OnCollisionEnter2D (Collision2D other);
+}
+public interface IWeapon {
+
+    void OnActivationTrigger ();
+}
